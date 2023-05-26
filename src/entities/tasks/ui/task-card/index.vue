619408a0bdf088269ba3b3ffa6c18afa2c1eb3bd
hoach-linux <hoachprogrammer@gmail.com>
2023-05-26 22:36:16 +0700

    add new style for listItem component

--- src/entities/tasks/ui/task-card/index.vue
+++ src/entities/tasks/ui/task-card/index.vue
@@ -1,48 +1,51 @@
 <template>
-  <ListItem
-    @dblclick="$router.push(`/task/${props.item.id}`)"
-    :class="styles.list_item"
-  >
-    <template #actions>
-      <slot name="actions" />
-    </template>
-    <ListItemMeta>
-      <template #title>
-        <a>{{ props.item.title }}</a>
-      </template>
-      <template #description>
-        <p>{{ props.item.description }}</p>
-        <div
-          v-if="props.item.date_start !== null && props.item.date_end !== null"
-        >
-          <Tag color="blue">
-            {{ itemStartDate }}
-          </Tag>
-          <Tag color="blue">
-            {{ itemEndDate }}
-          </Tag>
-        </div>
-        <div v-if="props.item.date_time !== null">
-          <Tag color="blue">
-            {{ itemDate }}
-          </Tag>
-        </div>
-      </template>
-    </ListItemMeta>
-  </ListItem>
+    <ListItem
+        @dblclick="$router.push(`/task/${props.item.id}`)"
+        :class="[styles.list_item, { [styles.completed]: item.checked }]"
+    >
+        <template #actions>
+            <slot name="actions" />
+        </template>
+        <ListItemMeta>
+            <template #title>
+                <a>{{ props.item.title }}</a>
+            </template>
+            <template #description>
+                <p>{{ props.item.description }}</p>
+                <div
+                    v-if="
+                        props.item.date_start !== null &&
+                        props.item.date_end !== null
+                    "
+                >
+                    <Tag color="blue">
+                        {{ itemStartDate }}
+                    </Tag>
+                    <Tag color="blue">
+                        {{ itemEndDate }}
+                    </Tag>
+                </div>
+                <div v-if="props.item.date_time !== null">
+                    <Tag color="blue">
+                        {{ itemDate }}
+                    </Tag>
+                </div>
+            </template>
+        </ListItemMeta>
+    </ListItem>
 </template>
 <script setup lang="ts">
 import { ListItemMeta, ListItem, Tag } from "ant-design-vue";
 import styles from "./styles.module.scss";
 
 interface IItem {
-  id: number;
-  title: string;
-  description: string;
-  checked: boolean;
-  date_start: string;
-  date_end: string;
-  date_time: string;
+    id: number;
+    title: string;
+    description: string;
+    checked: boolean;
+    date_start: string;
+    date_end: string;
+    date_time: string;
 }
 
 const props = defineProps<{ item: IItem }>();
--- src/features/toggle-task/ui/index.vue
+++ src/features/toggle-task/ui/index.vue
@@ -1,17 +1,17 @@
 <template>
-  <Checkbox
-    :checked="props.item.checked"
-    @click="taskStore.checkedTask(props.item)"
-  />
+    <Checkbox
+        :checked="props.item.checked"
+        @click="taskStore.checkedTask(props.item)"
+    />
 </template>
 <script setup lang="ts">
 import { Checkbox } from "ant-design-vue";
 import { taskFeatureCheckedModel } from "../../toggle-task";
 
 interface IItem {
-  title: string;
-  description: string;
-  checked: boolean;
+    title: string;
+    description: string;
+    checked: boolean;
 }
 
 const taskStore = taskFeatureCheckedModel();
