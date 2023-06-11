4a207f541c498632cbd901686ea8bbc7e50ac87b
Nguyen Tien Hoach <hoachnt@pop-os.localdomain>
2023-06-11 15:04:56 +0700

    redesign

--- src/entities/tasks/ui/task-card/index.vue
+++ src/entities/tasks/ui/task-card/index.vue
@@ -1,8 +1,6 @@
 <template>
-    <ListItem
-        @dblclick="$router.push(`/task/${props.item.id}`)"
-        :class="[styles.list_item, { [styles.completed]: item.checked }]"
-    >
+    <ListItem @dblclick="$router.push(`/task/${props.item.id}`)"
+        :class="[styles.list_item, { [styles.completed]: item.checked }]">
         <template #actions>
             <slot name="actions" />
         </template>
@@ -10,32 +8,11 @@
             <template #title>
                 <a>{{ props.item.title }}</a>
             </template>
-            <template #description>
-                <p>{{ props.item.description }}</p>
-                <div
-                    v-if="
-                        props.item.date_start !== null &&
-                        props.item.date_end !== null
-                    "
-                >
-                    <Tag color="blue">
-                        {{ itemStartDate }}
-                    </Tag>
-                    <Tag color="blue">
-                        {{ itemEndDate }}
-                    </Tag>
-                </div>
-                <div v-if="props.item.date_time !== null">
-                    <Tag color="blue">
-                        {{ itemDate }}
-                    </Tag>
-                </div>
-            </template>
         </ListItemMeta>
     </ListItem>
 </template>
 <script setup lang="ts">
-import { ListItemMeta, ListItem, Tag } from "ant-design-vue";
+import { ListItemMeta, ListItem } from "ant-design-vue";
 import styles from "./styles.module.scss";
 
 interface IItem {
@@ -49,12 +26,4 @@ interface IItem {
 }
 
 const props = defineProps<{ item: IItem }>();
-const userOffset = new Date().getTimezoneOffset() * 60000; // в миллисекундах
-let itemDate: Date | string = new Date(props.item.date_time);
-let itemStartDate: Date | string = new Date(props.item.date_start);
-let itemEndDate: Date | string = new Date(props.item.date_end);
-
-itemDate = new Date(itemDate.getTime() - userOffset).toLocaleString();
-itemStartDate = new Date(itemStartDate.getTime() - userOffset).toLocaleString();
-itemEndDate = new Date(itemEndDate.getTime() - userOffset).toLocaleString();
 </script>
