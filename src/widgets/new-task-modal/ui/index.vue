2f9a3cd80ba540adf62a9be3e140812f989513e2
hoach-linux <hoachprogrammer@gmail.com>
2023-09-12 15:48:37 +0700

    deleted unused transition

--- src/widgets/new-task-modal/ui/index.vue
+++ src/widgets/new-task-modal/ui/index.vue
@@ -7,63 +7,61 @@
     >
         <plus-outlined />
     </AButton>
-    <transition name="fade">
-        <LazyModal
-            v-if="modalStore.visible"
-            v-model:visible="modalStore.visible"
-            :title="'Create a task'"
-            :func-validation="() => (addTaskStore.validation = false)"
-            :func-close="modalStore.closeModal"
-        >
-            <template #footer>
-                <a-button key="back" @click="modalStore.closeModal"
-                    >Cancel</a-button
-                >
-                <AddTask @click="addTaskStore.addTask(modalStore.newTask)"
-                    >Create</AddTask
-                >
-            </template>
-            <Input
-                size="large"
-                v-model:value="modalStore.newTask.title"
-                placeholder="Title"
-                :class="styles.input"
-            />
-            <Input
-                size="large"
-                :class="styles.input"
-                v-model:value="modalStore.newTask.description"
-                placeholder="Description"
-            />
-            <DatePicker
-                v-if="
-                    modalStore.newTask.date === undefined ||
-                    modalStore.newTask.date === null
-                "
-                v-model:value="modalStore.newTask.date_time"
-                show-time
-                :class="styles.input"
-                size="large"
-            />
-            <RangePicker
-                v-if="
-                    modalStore.newTask.date_time === undefined ||
-                    modalStore.newTask.date_time === null
-                "
-                v-model:value="modalStore.newTask.date"
-                show-time
-                :class="styles.input"
-                size="large"
-            />
-            <Alert
-                v-if="addTaskStore.validation"
-                message="Title is empty"
-                type="error"
-                show-icon
-                :class="styles.error_alert"
-            />
-        </LazyModal>
-    </transition>
+    <LazyModal
+        v-if="modalStore.visible"
+        v-model:visible="modalStore.visible"
+        :title="'Create a task'"
+        :func-validation="() => (addTaskStore.validation = false)"
+        :func-close="modalStore.closeModal"
+    >
+        <template #footer>
+            <a-button key="back" @click="modalStore.closeModal"
+                >Cancel</a-button
+            >
+            <AddTask @click="addTaskStore.addTask(modalStore.newTask)"
+                >Create</AddTask
+            >
+        </template>
+        <Input
+            size="large"
+            v-model:value="modalStore.newTask.title"
+            placeholder="Title"
+            :class="styles.input"
+        />
+        <Input
+            size="large"
+            :class="styles.input"
+            v-model:value="modalStore.newTask.description"
+            placeholder="Description"
+        />
+        <DatePicker
+            v-if="
+                modalStore.newTask.date === undefined ||
+                modalStore.newTask.date === null
+            "
+            v-model:value="modalStore.newTask.date_time"
+            show-time
+            :class="styles.input"
+            size="large"
+        />
+        <RangePicker
+            v-if="
+                modalStore.newTask.date_time === undefined ||
+                modalStore.newTask.date_time === null
+            "
+            v-model:value="modalStore.newTask.date"
+            show-time
+            :class="styles.input"
+            size="large"
+        />
+        <Alert
+            v-if="addTaskStore.validation"
+            message="Title is empty"
+            type="error"
+            show-icon
+            :class="styles.error_alert"
+        />
+    </LazyModal>
 </template>
 <script setup lang="ts">
 import { Input, Alert, RangePicker, DatePicker } from "ant-design-vue";
